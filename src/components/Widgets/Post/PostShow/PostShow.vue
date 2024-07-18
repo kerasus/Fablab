50c1df579b8a36680a6d0c4f9f9080aff6eb8c9a
hgh <Haydar.Ghasemi@gmail.com>
2024-07-18 18:22:02 +0330

    وقتی بیلد میگرفتیم مقادیر محلی آپشن ها درست محاسبه نمیشد

--- src/components/Widgets/Post/PostShow/PostShow.vue
+++ src/components/Widgets/Post/PostShow/PostShow.vue
@@ -30,7 +30,7 @@
             {{post.category_info.parent.title}}
           </q-btn>
           <q-btn v-if="post.category_info?.id"
-                 :to="{name: this.localOptions.postCategoryShowRouteName , params: {category_id: post.category_info.id}}"
+                 :to="{name: localOptionsPostCategoryShowRouteNameComputed , params: {category_id: post.category_info.id}}"
                  class="attribute-item">
             {{post.category_info.title}}
           </q-btn>
@@ -52,24 +52,16 @@
 
 <script>
 import { Post } from 'src/models/Post.js'
-import ShamsiDate from 'src/assets/ShamsiDate.js'
+// import ShamsiDate from 'src/assets/ShamsiDate.js'
 import { APIGateway } from 'src/api/APIGateway.js'
 import { mixinPrefetchServerData, mixinWidget } from 'src/mixin/Mixins.js'
 import Breadcrumbs from 'src/components/Widgets/Breadcrumbs/Breadcrumbs.vue'
 
-let html2pdf
-// if (typeof window !== 'undefined') {
-//   import('html2pdf.js')
-//     .then((html2pdfLib) => {
-//       html2pdf = html2pdfLib.default
-//     })
-// }
-
 export default {
-  name: 'ShowClassroomInfo',
+  name: 'Public.PostShow',
   components: { Breadcrumbs },
   mixins: [mixinWidget, mixinPrefetchServerData],
-  emits: ['onloadn'],
+  // emits: ['onloadn'],
   data () {
     return {
       breadcrumbs: {
@@ -91,6 +83,9 @@ export default {
   computed: {
     postId () {
       return this.$route.params.id ?? this.$route.params.post_id
+    },
+    localOptionsPostCategoryShowRouteNameComputed() {
+      return this.localOptions.postCategoryShowRouteName
     }
   },
   methods: {
@@ -113,109 +108,14 @@ export default {
         to: { name: this.localOptions.postShowRouteName, params: { id: this.post.id } }
       })
       this.$store.commit('AppLayout/updateBreadcrumbs', this.breadcrumbs)
-      this.$emit('onloadn', this.post)
+      // this.$emit('onloadn', this.post)
     },
     prefetchServerDataPromiseCatch () {
       this.post.post = false
     },
-
-    getTitledDateTime (dateTime) {
-      return ShamsiDate.getTitledDateTime(dateTime)
-    },
-    getTime (time) {
-      return ShamsiDate.getDateTime(time)
-    },
     getPost () {
       this.post.post = true
       return APIGateway.post.get(this.postId)
-    },
-    print () {
-      this.convertPhotosToBase64()
-      const html2pdfConfig = {
-        margin: [0, 0, 0, 0],
-        image: {
-          type: 'jpeg',
-          quality: 0.6
-        },
-        filename: this.post.title,
-        html2canvas: {
-          dpi: 1,
-          scale: 2.5,
-          letterRendering: true,
-          useCORS: true
-        },
-        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
-      }
-      html2pdf()
-        .set(html2pdfConfig)
-        .from(this.$refs.printArea)
-        .save()
-        .thenExternal(() => {
-          this.downloadLoading = false
-        })
-    },
-    toDataURL(src, callback, outputFormat) {
-      const img = new Image()
-      img.crossOrigin = 'Anonymous'
-      img.onload = function() {
-        const canvas = document.createElement('CANVAS')
-        const ctx = canvas.getContext('2d')
-        canvas.height = this.naturalHeight
-        canvas.width = this.naturalWidth
-        ctx.drawImage(this, 0, 0)
-        const dataURL = canvas.toDataURL(outputFormat)
-        callback(dataURL)
-      }
-      if (src.includes('?')) {
-        img.src = src
-      } else {
-        img.src = src + '?test=123'
-      }
-      if (img.complete || img.complete === undefined) {
-        img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=='
-        img.src = src
-      }
-    },
-    convertPhotosToBase64 () {
-      this.convertSvgToBase64()
-      this.convertImagesToBase64()
-    },
-    convertImagesToBase64 () {
-      if (!this.$refs.printArea) {
-        return
-      }
-      const images = this.$refs.printArea.getElementsByTagName('img')
-      images.forEach(image => {
-        this.toDataURL(image.src, function(dataUrl) {
-          image.src = dataUrl
-        })
-      })
-    },
-    convertSvgToBase64 () {
-      if (!this.$refs.printArea) {
-        return
-      }
-      const svgs = this.$refs.printArea.getElementsByTagName('svg')
-      svgs.forEach(svg => {
-        // Convert the SVG node to HTML.
-        const div = document.createElement('div')
-        div.appendChild(svg.cloneNode(true))
-
-        // Encode the SVG as base64
-        const b64 = 'data:image/svg+xml;base64,' + window.btoa(div.innerHTML)
-        const url = 'url("' + b64 + '")'
-        svg.style.backgroundImage = url
-        svg.style.backgroundSize = 'cover'
-        if (svg.parentElement.classList.contains('brace-left')) {
-          svg.style.backgroundPosition = 'left'
-        }
-        if (svg.parentElement.classList.contains('brace-center')) {
-          svg.style.backgroundPosition = 'bottom'
-        }
-        if (svg.parentElement.classList.contains('brace-right')) {
-          svg.style.backgroundPosition = 'right'
-        }
-      })
     }
   }
 }
